72d3e713eee305bf3fda29ba943b533435d58604
Ohleh <94701344+Ohleh@users.noreply.github.com>
2024-03-26 12:57:33 +0000

    Deploying to gh-pages from @ Ohleh/js_9mh_date_promise_parcel@8517aedb59a2ca7debcd0c7b389b24120d7e7480 🚀

---
+++ 01-color-switcher.54fe7742.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),a=document.querySelector("body");let d;e.addEventListener("click",(()=>{e.disabled=!0,d=setInterval((()=>{a.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.addEventListener("click",(()=>{clearInterval(d),e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.54fe7742.js.map
